358abe766698e8bb9cd6cac0e436c7f34e7b0b13
jlbuild <juliabuildbot@gmail.com>
2026-02-19 19:56:25 +0000

    Qt6Base_jll build 6.10.2+0

--- src/Qt6Base_jll.jl
+++ src/Qt6Base_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6Base")
-JLLWrappers.@generate_main_file("Qt6Base", UUID("c0090381-4147-56d7-9ebc-da0b1113ec56"))
+JLLWrappers.@generate_main_file("Qt6Base", Base.UUID("c0090381-4147-56d7-9ebc-da0b1113ec56"))
 end  # module Qt6Base_jll
